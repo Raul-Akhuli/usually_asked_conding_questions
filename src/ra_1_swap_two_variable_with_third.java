0c9a74fa56430ad787c1d6de334094be6536126c
Raul <akhuliraul@gmail.com>
2025-10-09 22:07:58 +0530

    swap two variable with third: time complexity O(1) and space complexity O(1

--- src/ra_1_swap_two_variable_with_third.java
+++ src/ra_1_swap_two_variable_with_third.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class ra_1_swap_two_variable_with_third {
+public class ra_1_swap_two_variable_with_third{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter the value of a: ");
+        int a = sc.nextInt();
+
+        System.out.print("enter the value of b: ");
+        int b = sc.nextInt();
+
+        int temp = b;
+        b = a;
+        a = temp;
+
+        System.out.println("value of a is " + a + " value of b is: " + b);
+    }
 }
