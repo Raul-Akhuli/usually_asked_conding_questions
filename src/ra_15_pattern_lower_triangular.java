79bf8a33cf0620e3ac47967fd327115da2f91471
Raul <akhuliraul@gmail.com>
2025-10-16 22:57:25 +0530

    add file pattern lower triangular time complexity O(n^2) space complexity O(1)

---
+++ src/ra_15_pattern_lower_triangular.java
@@ -0,0 +1,26 @@
+/*
+when n value is 3
+
+*
+* *
+* * *
+
+*/
+
+import java.util.Scanner;
+
+public class ra_15_pattern_lower_triangular{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter no: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
