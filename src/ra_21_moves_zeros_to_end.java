2d3ec4f74e81dfaee2b91bb0793d5f71e298b5f0
Raul <akhuliraul@gmail.com>
2025-11-01 22:08:55 +0530

    add a file moves zeros to end having time complexity O(n) space complexity O(1)

---
+++ src/ra_21_moves_zeros_to_end.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ra_21_moves_zeros_to_end{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the element space separated: ");
+        String inputLine = sc.nextLine();
+        String [] stringArray = inputLine.split("\\s+");
+        int n = stringArray.length;
+
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(stringArray[i]);
+        }
+
+        int index = 0;
+
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] != 0) arr[index++] = arr[i];
+        }
+
+        while(index < n){
+            arr[index++] = 0;
+        }
+
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
+
